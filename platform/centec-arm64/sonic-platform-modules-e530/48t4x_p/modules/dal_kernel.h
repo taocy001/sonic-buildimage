bfaba114392aff90e19adde9bb0da3d1bae04233
taochengyi <taocy@centecnetworks.com>
2020-01-01 14:44:18 +0800

    bug fix

--- platform/centec-arm64/sonic-platform-modules-e530/48t4x_p/modules/dal_kernel.h
+++ platform/centec-arm64/sonic-platform-modules-e530/48t4x_p/modules/dal_kernel.h
@@ -85,10 +85,6 @@ struct dal_user_dev_s
 {
     unsigned int chip_num;   /*output: local chip number*/
     unsigned int lchip;       /*input: local chip id*/
-    unsigned int phy_base0; /* low 32bits physical base address */
-    unsigned int phy_base1; /* high 32bits physical base address */
-    unsigned int dma_phy_base0;
-    unsigned int dma_phy_base1;
     unsigned int bus_no;
     unsigned int dev_no;
     unsigned int fun_no;
