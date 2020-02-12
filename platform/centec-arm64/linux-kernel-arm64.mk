b7e2968ee7c94f220e7f7d664fac66d5af765493
taochengyi <taocy@centecnetworks.com>
2020-02-12 15:10:21 +0800

    compile linux kernel online for centec arm64 platform

--- platform/centec-arm64/linux-kernel-arm64.mk
+++ platform/centec-arm64/linux-kernel-arm64.mk
@@ -1,19 +1,8 @@
 # linux kernel package for centec arm64
 
-export KVERSION= 4.9.62
-export LINUX_KERNEL_VERSION=4.9.62_4.9.62
+KVERSION = 4.9.0-9-2-arm64
 
-LINUX_KERNEL = linux-image-4.9.62-arm64.deb
-$(LINUX_KERNEL)_URL = https://github.com/CentecNetworks/sonic-binaries/raw/master/$(PLATFORM_ARCH)/kernel/$(LINUX_KERNEL)
-SONIC_ONLINE_DEBS += $(LINUX_KERNEL)
-SONIC_STRETCH_DEBS += $(LINUX_KERNEL)
-
-LINUX_KERNEL_DEBIMG = linux-image-4.9.62_4.9.62-1_arm64.deb
-$(LINUX_KERNEL_DEBIMG)_URL = https://github.com/CentecNetworks/sonic-binaries/raw/master/$(PLATFORM_ARCH)/kernel/$(LINUX_KERNEL_DEBIMG)
-SONIC_ONLINE_DEBS += $(LINUX_KERNEL_DEBIMG)
-SONIC_STRETCH_DEBS += $(LINUX_KERNEL_DEBIMG)
-
-LINUX_KERNEL_HEADERS = linux-headers-4.9.62_4.9.62-1_arm64.deb
-$(LINUX_KERNEL_HEADERS)_URL = https://github.com/CentecNetworks/sonic-binaries/raw/master/$(PLATFORM_ARCH)/kernel/$(LINUX_KERNEL_HEADERS)
-SONIC_ONLINE_DEBS += $(LINUX_KERNEL_HEADERS)
-SONIC_STRETCH_DEBS += $(LINUX_KERNEL_HEADERS)
+LINUX_KERNEL_DTB_INITRAMFS = linux-dtb-initramfs.deb
+$(LINUX_KERNEL_DTB_INITRAMFS)_URL = https://github.com/CentecNetworks/sonic-binaries/raw/master/$(PLATFORM_ARCH)/kernel/$(LINUX_KERNEL_DTB_INITRAMFS)
+SONIC_ONLINE_DEBS += $(LINUX_KERNEL_DTB_INITRAMFS)
+SONIC_STRETCH_DEBS += $(LINUX_KERNEL_DTB_INITRAMFS)
