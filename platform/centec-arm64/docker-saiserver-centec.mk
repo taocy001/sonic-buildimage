8e4d99db3404eae4228c15c04491ff648a0d6e65
taochengyi <taocy@centecnetworks.com>
2020-02-25 03:59:58 +0000

    remove docker run option --net

--- platform/centec-arm64/docker-saiserver-centec.mk
+++ platform/centec-arm64/docker-saiserver-centec.mk
@@ -8,7 +8,7 @@ $(DOCKER_SAISERVER_CENTEC)_LOAD_DOCKERS += $(DOCKER_CONFIG_ENGINE_STRETCH)
 SONIC_DOCKER_IMAGES += $(DOCKER_SAISERVER_CENTEC)
 
 $(DOCKER_SAISERVER_CENTEC)_CONTAINER_NAME = saiserver
-$(DOCKER_SAISERVER_CENTEC)_RUN_OPT += --net=host --privileged -t
+$(DOCKER_SAISERVER_CENTEC)_RUN_OPT += --privileged -t
 $(DOCKER_SAISERVER_CENTEC)_RUN_OPT += -v /host/machine.conf:/etc/machine.conf
 $(DOCKER_SAISERVER_CENTEC)_RUN_OPT += -v /var/run/docker-saiserver:/var/run/sswsyncd
 $(DOCKER_SAISERVER_CENTEC)_RUN_OPT += -v /etc/sonic:/etc/sonic:ro
--- platform/centec-arm64/docker-syncd-centec.mk
+++ platform/centec-arm64/docker-syncd-centec.mk
@@ -11,7 +11,7 @@ $(DOCKER_SYNCD_BASE)_DBG_DEPENDS += $(SYNCD_DBG) \
                                 $(LIBSAIREDIS_DBG)
 
 $(DOCKER_SYNCD_BASE)_RUN_OPT += -v /host/warmboot:/var/warmboot
-$(DOCKER_SYNCD_CENTEC)_RUN_OPT += --net=host --privileged -t
+$(DOCKER_SYNCD_CENTEC)_RUN_OPT += --privileged -t
 $(DOCKER_SYNCD_CENTEC)_RUN_OPT += -v /host/machine.conf:/etc/machine.conf
 $(DOCKER_SYNCD_CENTEC)_RUN_OPT += -v /var/run/docker-syncd:/var/run/sswsyncd
 $(DOCKER_SYNCD_CENTEC)_RUN_OPT += -v /etc/sonic:/etc/sonic:ro
